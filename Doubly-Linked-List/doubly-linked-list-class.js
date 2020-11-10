ee326fe98a77ac43db798ca8b48a040891922118
dukhyunko93 <dukhyunko93@gmail.com>
2020-11-10 14:51:06 -0500

    push

--- Doubly-Linked-List/doubly-linked-list-class.js
+++ Doubly-Linked-List/doubly-linked-list-class.js
@@ -12,4 +12,18 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val){
+        let newNode = new Node(val);
+        if (!head){
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this
+    }
+   
 }
