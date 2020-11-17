3c0eb4f124783c814a38e07ff538dd1247377823
dukhyunko93 <dukhyunko93@gmail.com>
2020-11-17 13:09:54 -0500

    stacks intro

---
+++ Stacks/stacks-from-scratch.js
@@ -0,0 +1,42 @@
+// Linked List Implementation
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val)
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return console.log(++this.size);
+    }
+
+    pop(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+// node stacks-from-scratch
